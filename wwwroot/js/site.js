a3049a4574c1645acc66dc3ea905020df44f134e
Tran Thanh Binh <binhtthe171542@fpt.edu.vn>
2025-03-15 22:15:50 +0700

    Hoàn thành layout sơ bộ

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,36 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿document.addEventListener("DOMContentLoaded", function () {
+    var ctx = document.getElementById("myChart");
 
-// Write your JavaScript code.
+    if (ctx) {
+        var myChart = new Chart(ctx, {
+            type: "line",
+            data: {
+                labels: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+                datasets: [
+                    {
+                        data: [15339, 21345, 18483, 24003, 23489, 24092, 12034],
+                        lineTension: 0,
+                        backgroundColor: "transparent",
+                        borderColor: "#007bff",
+                        borderWidth: 4,
+                        pointBackgroundColor: "#007bff",
+                    },
+                ],
+            },
+            options: {
+                scales: {
+                    yAxes: [
+                        {
+                            ticks: {
+                                beginAtZero: false,
+                            },
+                        },
+                    ],
+                },
+                legend: {
+                    display: false,
+                },
+            },
+        });
+    }
+});
