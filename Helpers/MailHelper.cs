9a84a75188e6028a61123a92d26c64d3fb7499e0
Long Doan <longdhhe186717@fpt.edu.vn>
2025-03-16 15:22:56 +0700

    Register with email verification

---
+++ Helpers/MailHelper.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Net;
+using System.Net.Mail;
+
+namespace NettruyenRemake.Helpers
+{
+    public class MailHelper
+    {
+        private readonly IConfiguration _configuration;
+
+        public MailHelper(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public bool Send(string from, string to, string subject, string body)
+        {
+            try
+            {
+                var host = _configuration["Gmail:Host"];
+                var port = int.Parse(_configuration["Gmail:Port"]);
+                var username = _configuration["Gmail:Username"];
+                var password = _configuration["Gmail:Password"];
+                var enableSsl = bool.Parse(_configuration["Gmail:EnableSsl"]);
+                var smtpClient = new SmtpClient
+                {
+                    Host = host,
+                    Port = port,
+                    EnableSsl = enableSsl,
+                    Credentials = new NetworkCredential(username, password)
+                };
+                var mailMessage = new MailMessage(from, to);
+                mailMessage.Subject = subject;
+                mailMessage.Body = body;
+                smtpClient.Send(mailMessage);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
